ba04e785438b7b7d64eccd14fb0f1bf2b1189548
DenisValuev <valuev.denis91@mail.ru>
2024-09-09 13:28:58 +0300

    Добавьте файлы проекта.

---
+++ Module9.Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Module9.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptionsArray = 
+            {
+                new ArgumentException(),
+                new NullReferenceException(),
+                new MyException("Мое исключение"),
+                new IndexOutOfRangeException(),
+                new DivideByZeroException()
+            };
+            foreach (var item in exceptionsArray)
+            {
+                ProcessException(item);
+            }
+
+        }
+        static void ProcessException(Exception exception)
+        {
+            try
+            {
+                throw exception;
+            }
+            catch (NullReferenceException e) { Console.WriteLine(e.Message); }
+            catch (ArgumentException e) { Console.WriteLine(e.Message); }
+            catch (MyException e) { Console.WriteLine(e.Message); }
+            catch (IndexOutOfRangeException e) { Console.WriteLine(e.Message); }
+            catch (DivideByZeroException e) { Console.WriteLine(e.Message); }
+            catch (Exception e) { Console.WriteLine("Что то пошло не так"); }
+        }
+    }
+}
